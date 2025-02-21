39bbfbfee8ba1cdee51bf09a677dbdfad6f9b04d
Santanu Mondal <54280464+iamsantanu21@users.noreply.github.com>
2025-02-21 15:41:51 +0530

    main

--- Server/ChatServerMain.java
+++ Server/ChatServerMain.java
@@ -1,17 +1,13 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 public class ChatServerMain {
     public static void main(String[] args) {
         try {
             ChatServer server = new ChatServerImpl();
-            ChatServer stub = (ChatServer) UnicastRemoteObject.exportObject(server, 0);
-
             Registry registry = LocateRegistry.createRegistry(4000);
-            registry.rebind("ChatServer", stub);
-
-            System.out.println("Chat server is running on 10.10.32.115:4000...");
+            registry.rebind("ChatServer", server);
+            System.out.println("Chat server is running...");
         } catch (Exception e) {
             e.printStackTrace();
         }
