3f24f8f8cf8148aa9b500397093bdc40858b59ca
Santanu Mondal <54280464+iamsantanu21@users.noreply.github.com>
2025-02-21 18:51:25 +0530

    main

--- Server/ChatServerMain.java
+++ Server/ChatServerMain.java
@@ -5,7 +5,7 @@ public class ChatServerMain {
     public static void main(String[] args) {
         try {
             ChatServer server = new ChatServerImpl();
-            Registry registry = LocateRegistry.createRegistry(4000);
+            Registry registry = LocateRegistry.createRegistry(5000);
             registry.rebind("ChatServer", server);
             System.out.println("Chat server is running...");
         } catch (Exception e) {
